59744fbad2a51c1da4d40e50173d3f9913eda7cc
Daniel Kenyon-Jones <github@dlkj.co.uk>
2023-05-06 13:34:03 +0100

    add real time clock, hardware random seed and allow server to start additional sessions

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,19 @@
 #![allow(clippy::struct_excessive_bools)]
 #![warn(clippy::use_self)]
 
-use core::fmt::Write;
-
 use bsp::entry;
-use bsp::hal;
-use defmt::debug;
-use defmt::{info, warn};
+use bsp::hal::{self, rosc::RingOscillator, Timer};
+use defmt::{debug, warn};
 use defmt_rtt as _;
 use hal::{clocks::init_clocks_and_plls, pac, watchdog::Watchdog};
+use heapless::Vec;
 use panic_probe as _;
 use rp_pico as bsp;
-use smoltcp::socket::dhcpv4;
-use smoltcp::wire::Ipv4Cidr;
 use smoltcp::{
     iface::{Config, Interface, SocketSet},
-    socket::tcp,
-    time::{Duration, Instant},
-    wire::{EthernetAddress, IpAddress, IpCidr, Ipv4Address},
+    socket::{dhcpv4, tcp},
+    time::Instant,
+    wire::{EthernetAddress, IpAddress, IpCidr, Ipv4Address, Ipv4Cidr},
 };
 #[allow(clippy::wildcard_imports)]
 use usb_device::class_prelude::*;
@@ -39,9 +35,10 @@ mod usbd_ethernet;
 
 #[entry]
 fn main() -> ! {
-    info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+    let ring_oscillator = RingOscillator::new(pac.ROSC).initialize();
 
     let clocks = init_clocks_and_plls(
         bsp::XOSC_CRYSTAL_FREQ,
@@ -77,8 +74,7 @@ fn main() -> ! {
     let our_mac_addr = [0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC];
     let mut config = Config::new();
     config.hardware_addr = Some(EthernetAddress(our_mac_addr).into());
-    // Todo set random seed
-    // config.random_seed = todo!();
+    config.random_seed = generate_random_seed(&ring_oscillator);
 
     let mut iface = Interface::new(config, &mut cdc_ncm);
     iface.update_ip_addrs(|ip_addrs| {
@@ -92,7 +88,7 @@ fn main() -> ! {
         .unwrap();
 
     // Create sockets
-    let mut dhcp_socket = dhcpv4::Socket::new();
+    let dhcp_socket = dhcpv4::Socket::new();
 
     let server_socket = {
         // It is not strictly necessary to use a `static mut` and unsafe code here, but
@@ -101,9 +97,9 @@ fn main() -> ! {
         // when stack overflows.
         static mut TCP_SERVER_RX_DATA: [u8; 1024] = [0; 1024];
         static mut TCP_SERVER_TX_DATA: [u8; 1024] = [0; 1024];
-        let tcp_rx_buffer = tcp::SocketBuffer::new(unsafe { &mut TCP_SERVER_RX_DATA[..] });
-        let tcp_tx_buffer = tcp::SocketBuffer::new(unsafe { &mut TCP_SERVER_TX_DATA[..] });
-        tcp::Socket::new(tcp_rx_buffer, tcp_tx_buffer)
+        let tcp_receive_buffer = tcp::SocketBuffer::new(unsafe { &mut TCP_SERVER_RX_DATA[..] });
+        let tcp_transmit_buffer = tcp::SocketBuffer::new(unsafe { &mut TCP_SERVER_TX_DATA[..] });
+        tcp::Socket::new(tcp_receive_buffer, tcp_transmit_buffer)
     };
 
     let mut sockets: [_; 2] = Default::default();
@@ -111,61 +107,83 @@ fn main() -> ! {
     let server_handle = sockets.add(server_socket);
     let dhcp_handle = sockets.add(dhcp_socket);
 
-    let mut timestamp = Instant::ZERO;
     loop {
-        usb_dev.poll(&mut [&mut cdc_ncm]);
-        iface.poll(timestamp, &mut cdc_ncm, &mut sockets);
-
-        // Todo replace with hardware clock
-        timestamp += Duration::from_micros(100);
-
-        if cdc_ncm.state() == State::Configured && cdc_ncm.data_if_enabled() {
+        if usb_dev.poll(&mut [&mut cdc_ncm])
+            && cdc_ncm.state() == State::Configured
+            && cdc_ncm.data_if_enabled()
+        {
             cdc_ncm.connect().ok();
         }
-
-        let socket = sockets.get_mut::<tcp::Socket>(server_handle);
-        if !socket.is_active() && !socket.is_listening() {
-            debug!("listening on 1234");
-            socket.listen(1234).unwrap();
+        let timestamp = Instant::from_micros(i64::try_from(timer.get_counter().ticks()).unwrap());
+        if iface.poll(timestamp, &mut cdc_ncm, &mut sockets) {
+            server_poll(sockets.get_mut::<tcp::Socket>(server_handle));
+            dhcp_poll(&mut iface, sockets.get_mut::<dhcpv4::Socket>(dhcp_handle));
         }
+    }
+}
 
-        if socket.can_recv() {
-            let r: Result<&str, core::str::Utf8Error> = socket
-                .recv(|buffer| (buffer.len(), core::str::from_utf8(buffer)))
-                .unwrap();
-            if let Ok(s) = r {
-                debug!("got {:?}", s);
-                socket.write_str("*").unwrap();
-            }
-            //socket.close();
+fn generate_random_seed(ring_oscillator: &RingOscillator<hal::rosc::Enabled>) -> u64 {
+    let mut seed = 0u64;
+    for i in 0..64 {
+        seed += u64::from(ring_oscillator.get_random_bit()) << i;
+    }
+    debug!("seed: {}", seed);
+    seed
+}
+
+fn server_poll(socket: &mut tcp::Socket) {
+    if !socket.is_open() {
+        debug!("listening on 1234");
+        socket.listen(1234).unwrap();
+    }
+
+    if socket.may_recv() {
+        let mut data = Vec::<u8, 1024>::new();
+        socket
+            .recv(|buffer| {
+                if !buffer.is_empty() {
+                    debug!("recv data: {:?}", buffer);
+                }
+                // Echo the data back in upper case
+                data.extend(buffer.iter().copied().map(|c| c.to_ascii_uppercase()));
+                (buffer.len(), ())
+            })
+            .unwrap();
+        if socket.can_send() && !data.is_empty() {
+            socket.send_slice(&data[..]).unwrap();
         }
+    } else if socket.may_send() {
+        debug!("socket close");
+        socket.close();
+    }
+}
 
-        let event = sockets.get_mut::<dhcpv4::Socket>(dhcp_handle).poll();
-        match event {
-            None => {}
-            Some(dhcpv4::Event::Configured(config)) => {
-                debug!("DHCP config acquired!");
+fn dhcp_poll(iface: &mut Interface, socket: &mut dhcpv4::Socket) {
+    let event = socket.poll();
+    match event {
+        None => {}
+        Some(dhcpv4::Event::Configured(config)) => {
+            debug!("DHCP config acquired!");
 
-                debug!("IP address:      {}", config.address);
-                set_ipv4_addr(&mut iface, config.address);
+            debug!("IP address:      {}", config.address);
+            set_ipv4_addr(iface, config.address);
 
-                if let Some(router) = config.router {
-                    debug!("Default gateway: {}", router);
-                    iface.routes_mut().add_default_ipv4_route(router).unwrap();
-                } else {
-                    debug!("Default gateway: None");
-                    iface.routes_mut().remove_default_ipv4_route();
-                }
-
-                for (i, s) in config.dns_servers.iter().enumerate() {
-                    debug!("DNS server {}:    {}", i, s);
-                }
-            }
-            Some(dhcpv4::Event::Deconfigured) => {
-                debug!("DHCP lost config!");
-                set_ipv4_addr(&mut iface, Ipv4Cidr::new(Ipv4Address::UNSPECIFIED, 0));
+            if let Some(router) = config.router {
+                debug!("Default gateway: {}", router);
+                iface.routes_mut().add_default_ipv4_route(router).unwrap();
+            } else {
+                debug!("Default gateway: None");
                 iface.routes_mut().remove_default_ipv4_route();
             }
+
+            for (i, s) in config.dns_servers.iter().enumerate() {
+                debug!("DNS server {}:    {}", i, s);
+            }
+        }
+        Some(dhcpv4::Event::Deconfigured) => {
+            debug!("DHCP lost config!");
+            set_ipv4_addr(iface, Ipv4Cidr::new(Ipv4Address::UNSPECIFIED, 0));
+            iface.routes_mut().remove_default_ipv4_route();
         }
     }
 }
